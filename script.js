b8a774c4ca4f0c591f28394b40733f7e553136ec
lonsoss <75281787+lonsoss@users.noreply.github.com>
2022-06-25 11:46:05 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,42 @@
+
+
+    document.querySelector('.form').addEventListener('change',function(){
+    document.querySelector('.hiden').style.display="block"
+
+    let facture = Number(document.querySelector('.facture').value)
+    let pourboire = Number(document.querySelector('.pourboir_input').value)
+
+    document.querySelector('.pourboire_affiche').innerHTML = `${pourboire}%`  
+
+    let pourboireDisplay = facture * pourboire / 100
+    let factureTotal = facture + pourboireDisplay
+
+    document.querySelector('.pourboire_display').placeholder = `${pourboireDisplay.toFixed(2)} $` 
+    document.querySelector('.facture_display').placeholder = `${factureTotal.toFixed(2)} $` 
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
